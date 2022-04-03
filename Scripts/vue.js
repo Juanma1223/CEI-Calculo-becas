9f2ba7a890963b0d6a3815936adac422eedd8d88
francosorbello <franco.sorbelo@gmail.com>
2022-04-03 11:46:48 -0300

    app reads filter csv

--- Scripts/vue.js
+++ Scripts/vue.js
@@ -30,8 +30,18 @@ Vue.createApp({
             reader.readAsText(fileUpload.files[0]);
 
         },
-        getWitghts() {
+        getWeights() {
             // Aca se decodifica el archivo con los pesos de la formula
+            var fileUpload = document.getElementById("filePesos")
+            const reader = new FileReader();
+            var rawData;
+            reader.addEventListener('load', (event) => {
+                rawData = event.target.result;
+                var table = Papa.parse(rawData);
+                console.log(table)
+            });
+            reader.readAsText(fileUpload.files[0]);
+
         },
         calculate() {
             // Aca se calculan las posiciones de los alumnos y settea las variables globales
@@ -47,9 +57,11 @@ Vue.createApp({
 
             this.columns.forEach(element => {
                 if(element === "") return;
+
                 filters.push(element);
                 filters.push("");
             });
+
             var parsedFilters = Papa.unparse([filters])
             const blob = new Blob([parsedFilters], {type: "text/csv"})
             const url = window.URL.createObjectURL(blob)
